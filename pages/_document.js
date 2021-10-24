3ef3ee2b02febeebd88970ec36cbd17e3a9be9f3
Akramtello <35160721+akram-tello@users.noreply.github.com>
2021-10-24 10:10:51 +0800

    set up PWA

---
+++ pages/_document.js
@@ -0,0 +1,35 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+    static async getInitialProps(ctx) {
+        const initialProps = await Document.getInitialProps(ctx)
+        return { ...initialProps }
+    }
+
+    render() {
+        return (
+            <Html lang="en">
+                <Head>
+                    <title>FlowyHire</title>
+                    <link rel="icon" href="/favicon.ico" />
+                    <meta name='application-name' content='FlowyHire' />
+                    <meta name='apple-mobile-web-app-capable' content='yes' />
+                    <meta name='apple-mobile-web-app-status-bar-style' content='default' />
+                    <meta name='apple-mobile-web-app-title' content='FlowyHire' />
+                    <meta name='description' content='Hire anyone, anywhere in a new country in minutes' />
+                    <meta name='format-detection' content='telephone=no' />
+                    <meta name='mobile-web-app-capable' content='yes' />
+                    <link rel="manifest" href="/manifest.json" />
+                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous" />
+                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossOrigin="anonymous"></script>
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
+}
+
+export default MyDocument
