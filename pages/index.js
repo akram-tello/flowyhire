c1eb8f00e97e90420a9758cb5b7b5baf91656d53
Akramtello <35160721+akram-tello@users.noreply.github.com>
2021-10-25 10:44:05 +0800

    add pricing page

--- pages/index.js
+++ pages/index.js
@@ -1,46 +1,11 @@
-import { useUser } from '@auth0/nextjs-auth0'
+import Navbar from '../components/Navbar'
 import home from '../styles/Home.module.scss'
 
 
 export default function Home() {
-  const { user } = useUser();
   return (
     <>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
-          <h3>Flowyhire</h3>
-          {/* <a class="navbar-brand logo" href="#"></a> */}
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav ms-md-auto gap-2">
-              {/* only show login if there is no user */}
-              {!user && (
-                <>
-                  <li className="nav-item rounded">
-                    <a className="nav-link active" aria-current="page" href="#"><i className="bi bi-house-fill me-2"></i>Home</a>
-                  </li>
-                  <li className="nav-item rounded">
-                    <a className="btn btn-outline-secondary" href="/api/auth/login"><i className="bi bi-telephone-fill me-2"></i>login</a>
-                  </li>
-                </>
-              )}
-              {/* if there is user, show the user info and logout btn */}
-              {user && (
-                <>
-                  <img src={user.picture} className="rounded-circle" width="50" height="50" />
-                  <p>{user.name}</p>
-                  <li className="nav-item rounded">
-                    <a className="btn btn-outline-danger" href="/api/auth/logout"><i className="bi bi-telephone-fill me-2"></i>logout</a>
-                  </li>
-                </>
-              )}
-
-            </ul>
-          </div>
-        </div>
-      </nav>
+      <Navbar />
       <div className="vh-100 d-flex justify-content-center align-items-center">
         <main>
           <h1 className={`${home.red} ${home.t1}`}>
---
+++ pages/pricing.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+const pricing = () => {
+    return (
+        <>
+            <Navbar />
+            <h2>pricing</h2>
+        </>
+    )
+}
+
+export default pricing
