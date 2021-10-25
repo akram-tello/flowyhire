080cb87b6dadc94451ffdd2e16b076bc66d00c21
Akramtello <35160721+akram-tello@users.noreply.github.com>
2021-10-25 13:57:32 +0800

    update

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from '../Navbar'
 import Footer from '../footer'
-import Styles from './Layout.module.scss'
+// import Styles from './Layout.module.scss'
 const Layout = ({ children }) => {
     return (
         <>
