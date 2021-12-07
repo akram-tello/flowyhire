2ef91ffb75d0f7635fcb3655c86aa631d8e796f9
Akramtello <35160721+akram-tello@users.noreply.github.com>
2021-12-07 21:42:55 +0800

    fix PWA

--- pages/_document.js
+++ pages/_document.js
@@ -20,6 +20,8 @@ class MyDocument extends Document {
                     <meta name='format-detection' content='telephone=no' />
                     <meta name='mobile-web-app-capable' content='yes' />
                     <link rel="manifest" href="/manifest.json" />
+                    <link rel="apple-touch-icon" href="/icon.png"></link>
+                    <meta name="theme-color" content="#fff" />
                     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous" />
                     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossOrigin="anonymous"></script>
                 </Head>
